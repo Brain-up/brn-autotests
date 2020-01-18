1323f7e61c6bb3f65e520165e99916fbe3a5c819
Kseniia_Stepanova <kseniia_stepanova@epam.com>
2020-01-19 00:07:51 +0300

    fix Test annotation description

--- src/test/java/ExerciseContractTests.java
+++ src/test/java/ExerciseContractTests.java
@@ -15,7 +15,7 @@ public class ExerciseContractTests extends InitTest {
         rspecExercise = init.setRequestSpecExercises();
     }
 
-    @Test(description = "/exercises")
+    @Test(description = "/exercises?seriesId={id}")
     public void checkExerciseContract() {
         given().spec(rspecExercise).when().queryParam("seriesId", "1").get("").then()
                 .assertThat().body(matchesJsonSchemaInClasspath("schemas/exercises_for_single_words.json")).statusCode(200);
