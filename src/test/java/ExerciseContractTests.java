b9d51deff5319cbf8c93841b5e0659d291620fb5
Kseniia_Stepanova <kseniia_stepanova@epam.com>
2020-01-20 13:03:54 +0300

    added contract tests for /exercise endpoints

--- src/test/java/ExerciseContractTests.java
+++ src/test/java/ExerciseContractTests.java
@@ -16,17 +16,45 @@ public class ExerciseContractTests extends InitTest {
         rspecExercise = init.setRequestSpecExercises();
     }
 
-    @Test(description = "/exercises?seriesId={id}")
+    @Test(description = "single words series /exercises?seriesId={id}")
     public void checkExerciseContract() {
         given().spec(rspecExercise).when().queryParam("seriesId", "1").get("").then()
                 .assertThat().body(matchesJsonSchemaInClasspath("schemas/exercises_for_single_words.json")).statusCode(200);
 
     }
 
-    @Test(description = "/exercises/{exerciseId}")
+    @Test(description = "single words series /exercises?seriesId={id}&userId={id}")
     public void checkExerciseContract_2() {
+        given().spec(rspecExercise).when().queryParam("seriesId", "1").queryParam("userId", "1").get("").then()
+                .assertThat().body(matchesJsonSchemaInClasspath("schemas/exercises_for_single_words.json")).statusCode(200);
+
+    }
+
+    @Test(description = "sequence words series /exercises?seriesId={id}")
+    public void checkExerciseContract_3() {
+        given().spec(rspecExercise).when().queryParam("seriesId", "2").get("").then()
+                .assertThat().body(matchesJsonSchemaInClasspath("schemas/exercises_for_sequence_words.json")).statusCode(200);
+
+    }
+
+    @Test(description = "sequence words series /exercises?seriesId={id}&userId={id}")
+    public void checkExerciseContract_4() {
+        given().spec(rspecExercise).when().queryParam("seriesId", "2").queryParam("userId", "1").get("").then()
+                .assertThat().body(matchesJsonSchemaInClasspath("schemas/exercises_for_sequence_words.json")).statusCode(200);
+
+    }
+
+    @Test(description = "single words series /exercises/{exerciseId}")
+    public void checkExerciseContract_5() {
         given().spec(rspecExercise).when().get("/1").then()
-                .assertThat().body(matchesJsonSchemaInClasspath("schemas/exercise_for_single_words.json")).statusCode(200);
+                .assertThat().body(matchesJsonSchemaInClasspath("schemas/exercise.json")).statusCode(200);
+
+    }
+
+    @Test(description = "sequence words series /exercises/{exerciseId}")
+    public void checkExerciseContract_6() {
+        given().spec(rspecExercise).when().get("/2").then()
+                .assertThat().body(matchesJsonSchemaInClasspath("schemas/exercise.json")).statusCode(200);
 
     }
 }
